37fd56447c546e1edc508e42d3bcc780d5d26ae0
mikary <mikary@microsoft.com>
2015-08-04 16:25:32 -0700

    Fix #290 Query DI Part 1 - CompiledQueryCache and Database classes Move compilation code out of cache to allow for scoped injection Decouple Database classes from query compilation, eliminates need for provider- specific relational Database classes

---
+++ src/EntityFramework.Core/Query/QueryExecutor.cs
@@ -0,0 +1,75 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Microsoft.Data.Entity.Utilities;
+using System.Linq;
+
+namespace Microsoft.Data.Entity.Query
+{
+    public class QueryExecutor : IQueryExecutor
+    {
+        private readonly IQueryContextFactory _contextFactory;
+        private readonly IQueryPreprocessor _preprocessor;
+        private readonly IQueryCompiler _compiler;
+
+        public QueryExecutor(
+            [NotNull] IQueryContextFactory contextFactory,
+            [NotNull] IQueryPreprocessor preprocessor,
+            [NotNull] IQueryCompiler compiler)
+        {
+            Check.NotNull(contextFactory, nameof(contextFactory));
+            Check.NotNull(preprocessor, nameof(preprocessor));
+            Check.NotNull(compiler, nameof(compiler));
+
+            _contextFactory = contextFactory;
+            _preprocessor = preprocessor;
+            _compiler = compiler;
+        }
+
+        public virtual TResult Execute<TResult>([NotNull] Expression query)
+        {
+            Check.NotNull(query, nameof(query));
+
+            var queryContext = _contextFactory.Create();
+
+            query = _preprocessor.Preprocess(query, queryContext);
+
+            var compiledQuery = _compiler.CompileQuery<TResult>(query);
+
+            return compiledQuery(queryContext);
+        }
+
+        public virtual IAsyncEnumerable<TResult> ExecuteAsync<TResult>([NotNull] Expression query)
+        {
+            Check.NotNull(query, nameof(query));
+
+            var queryContext = _contextFactory.Create();
+
+            query = _preprocessor.Preprocess(query, queryContext);
+
+            var compiledQuery = _compiler.CompileAsyncQuery<TResult>(query);
+
+            return compiledQuery(queryContext);
+        }
+
+        public virtual Task<TResult> ExecuteAsync<TResult>([NotNull] Expression query, CancellationToken cancellationToken)
+        {
+            Check.NotNull(query, nameof(query));
+
+            var queryContext = _contextFactory.Create();
+            queryContext.CancellationToken = cancellationToken;
+
+            query = _preprocessor.Preprocess(query, queryContext);
+
+            var compiledQuery = _compiler.CompileAsyncQuery<TResult>(query);
+
+            return compiledQuery(queryContext)
+                .First(cancellationToken);
+        }
+    }
+}
---
+++ src/EntityFramework.Core/Query/QueryPreprocessor.cs
@@ -0,0 +1,38 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Linq.Expressions;
+using JetBrains.Annotations;
+using Microsoft.Data.Entity.Query.ExpressionVisitors;
+using Microsoft.Data.Entity.Utilities;
+using Remotion.Linq.Parsing.ExpressionVisitors.TreeEvaluation;
+
+namespace Microsoft.Data.Entity.Query
+{
+    public class QueryPreprocessor : IQueryPreprocessor
+    {
+        public virtual Expression Preprocess([NotNull] Expression query, [NotNull] QueryContext queryContext)
+        {
+            Check.NotNull(query, nameof(query));
+            Check.NotNull(queryContext, nameof(queryContext));
+
+            var annotatedQuery = new QueryAnnotatingExpressionVisitor()
+                .Visit(query);
+
+            var functionEvaluationDisabledExpression = new FunctionEvaluationDisablingVisitor()
+                .Visit(annotatedQuery);
+
+            var partialEvaluationInfo = EvaluatableTreeFindingExpressionVisitor
+                .Analyze(functionEvaluationDisabledExpression, new NullEvaluatableExpressionFilter());
+
+            var parameterExtractedExpression = new ParameterExtractingExpressionVisitor(partialEvaluationInfo, queryContext)
+                .Visit(functionEvaluationDisabledExpression);
+
+            return parameterExtractedExpression;
+        }
+
+        private class NullEvaluatableExpressionFilter : EvaluatableExpressionFilterBase
+        {
+        }
+    }
+}
